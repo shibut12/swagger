83d07d15e4b6bc450903127a2a4e68567b5619ec
Shibu <shibu@hotmail.co.in>
2018-01-19 10:21:31 -0500

    Updated with a functioning api controller, added XML comments

---
+++ aspnet-webapi/sample-web-api-vs-2013/sample-web-api-vs-2013/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample_web_api_vs_2013.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
